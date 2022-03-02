6e2acc46dfc0a84e5694197211ed117f344dd181
Smlcrp <smlcrp@gmail.com>
2022-03-02 18:33:02 -0500

    able to create new cheeses on react app

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,20 +4,36 @@ import Cheese from '../pages/Cheese';
 import Show from '../pages/Show';
 
 function Main(props) {
+    const [cheese, setCheese] = useState(null);
+
+    const URL = 'https://cheesey-app.herokuapp.com/cheese';
+
+    const getCheese = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setCheese(data);
+    };
+
+    const createCheese = async (cheese) => {
+        await fetch(URL, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cheese),
+        });
+        getCheese();
+    };
+
+    useEffect(() => getCheese(), []);
+
     return(
         <main>
             <Switch>
                 <Route exact path='/'>
-                    <Cheese/>
+                    <Cheese cheese={cheese} createCheese={createCheese}/>
                 </Route>
-                <Route
-                    path='/cheese/:id'
-                    render={(rp) => (
-                        <Show
-                            {...rp}
-                        />
-                    )}
-                />
+                <Route path='/cheese/:id' render={(rp) => (<Show {...rp}/>)}/>
             </Switch>
         </main>
     );
