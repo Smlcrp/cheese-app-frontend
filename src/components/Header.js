82131d36b4ab6853a54caaec025757d219e37047
Smlcrp <smlcrp@gmail.com>
2022-03-02 17:42:41 -0500

    got stuff to show up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Header(props) {
-    return <h1>Header</h1>
-};
+
+	return (
+
+		<nav className="nav">
+
+			<Link to="/">
+
+				<div>Cheese Any Cheese</div>
+
+			</Link>
+
+		</nav>
+	);
+}
 
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Cheese from '../pages/Cheese';
+import Show from '../pages/Show';
+
 function Main(props) {
-    return <h1>Main</h1>
-};
+    return(
+        <main>
+            <Switch>
+                <Route exact path='/'>
+                    <Cheese/>
+                </Route>
+                <Route
+                    path='/cheese/:id'
+                    render={(rp) => (
+                        <Show
+                            {...rp}
+                        />
+                    )}
+                />
+            </Switch>
+        </main>
+    );
+}
 
 export default Main;
